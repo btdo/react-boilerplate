ee04033feb1d364e12b9d6398c70f71f570ad49a
bao.do <bao.do@innovapost.com>
2018-10-15 22:14:34 -0400

    Init commit

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import {Header} from '../../components/Header';
+import {shallow} from 'enzyme';
+
+test('should render header correctly', () => {
+    const wrapper =  shallow(<Header startLogout={() => {}}/>);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper =  shallow(<Header startLogout={startLogout}/>);
+    wrapper.find('button').simulate('click', {
+        preventDefault: () => {}
+    });
+    expect(startLogout).toHaveBeenCalled();
+})
